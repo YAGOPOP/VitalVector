ac1f81a73e00e025908054b1fea56929f237dc79
YAGOPOP <ttatarinov50@gmail.com>
2024-03-02 19:55:47 +0300

    0.46.1.3.5

---
+++ forssScreen.py
@@ -0,0 +1,51 @@
+from kivy.uix.boxlayout import BoxLayout
+from kivy.properties import ObjectProperty
+from kivy.properties import StringProperty
+from kivy.properties import ListProperty
+from kivy.lang import Builder
+
+class forssScreen(BoxLayout):
+    Builder.load_file('VitalVectorDesign.kv')
+
+    backolor = ListProperty([.82, .72, .37, 1])
+
+
+    currentAgeValue = 0
+    current4SSValue = 0
+
+    relativeFontSize = 20
+
+    ButtonsFontSize = StringProperty(str(relativeFontSize) + "sp")
+    LabelsFontSize = StringProperty(str(relativeFontSize * 2) + "sp")
+
+
+    ageLabel = ObjectProperty()
+    forSSLabel = ObjectProperty()
+    written4SSLabel = ObjectProperty()
+    writtenAgeLabel = ObjectProperty()
+
+
+    def updateLabels(self):
+        if self.currentAgeValue == 0:
+            self.current4SSValue = 0
+        self.ageLabel.text = str(self.currentAgeValue)
+        self.forSSLabel.text = str(self.current4SSValue)
+
+
+    def countAgeAnd4SS(self, instance):
+        self.currentAgeValue = int(str(self.currentAgeValue) + instance.text)
+        self.current4SSValue = round((220 - self.currentAgeValue) * 0.85)
+        self.updateLabels()
+
+    def clear(self, instance):
+        self.currentAgeValue = 0
+        self.current4SSValue = 0
+        self.updateLabels()
+
+    def DeleteLast(self, instance):
+        if self.currentAgeValue > 9:
+            self.currentAgeValue = int(str(self.currentAgeValue)[0:-1:])
+        else:
+            self.currentAgeValue = 0
+        self.current4SSValue = round((220 - self.currentAgeValue) * 0.85)
+        self.updateLabels()
--- main.py
+++ main.py
@@ -1,49 +1,10 @@
 from kivy.app import App
-from kivy.uix.boxlayout import BoxLayout
-from kivy.properties import ObjectProperty
-from kivy.properties import StringProperty
-from kivy.metrics import Metrics
-from kivy.lang import Builder
-
-__version__ = "0.46.1.2"
-
-class forssScreen(BoxLayout):
-    Builder.load_file('VitalVectorDesign.kv')
+from forssScreen import forssScreen
 
 
-    currentAgeValue = 0
-    current4SSValue = 0
-
-    ageLabel = ObjectProperty()
-    forSSLabel = ObjectProperty()
-
-    ButtonsFontSize = StringProperty("30sp")
-    LabelsFontSize = StringProperty("60sp")
-
-    def updateLabels(self):
-        if self.currentAgeValue == 0:
-            self.current4SSValue = 0
-        self.ageLabel.text = str(self.currentAgeValue)
-        self.forSSLabel.text = str(self.current4SSValue)
+__version__ = "0.46.1.4"
 
 
-    def countAgeAnd4SS(self, instance):
-        self.currentAgeValue = int(str(self.currentAgeValue) + instance.text)
-        self.current4SSValue = round((220 - self.currentAgeValue) * 0.85)
-        self.updateLabels()
-
-    def clear(self, instance):
-        self.currentAgeValue = 0
-        self.current4SSValue = 0
-        self.updateLabels()
-
-    def DeleteLast(self, instance):
-        if self.currentAgeValue > 9:
-            self.currentAgeValue = int(str(self.currentAgeValue)[0:-1:])
-        else:
-            self.currentAgeValue = 0
-        self.current4SSValue = round((220 - self.currentAgeValue) * 0.85)
-        self.updateLabels()
 
 
 class VitalVector(App):
