ad05dc3b85fbe06fdcf94cfd0ede366808a56320
YAGOPOP <ttatarinov50@gmail.com>
2024-03-18 20:28:32 +0300

    0.90.0.0

--- main.py
+++ main.py
@@ -3,17 +3,16 @@ from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
 from EkgsScreen import *
 from ForssScreen import *
-from SettingsScreen import *
 
-__version__ = "0.47.2.1"
+__version__ = "0.90.0.0"
 
 class VitalVector(App):
     def build(self):
         sm = ScreenManager()
         sm.add_widget(ForssScreen())
         sm.add_widget(EkgsScreen())
-        # sm.add_widget(SettingsScreen())
         return sm
+    
 
 if __name__ == '__main__':
     VitalVector().run()
